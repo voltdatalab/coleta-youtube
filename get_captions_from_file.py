53ff72ebea43c5b861b956dba4dcc5e53218dfb3
Lucas Lago <lago@nic.br>
2022-08-03 21:55:22 -0300

    working with new structur

--- get_captions_from_file.py
+++ get_captions_from_file.py
@@ -4,34 +4,37 @@ from database import insere_captions
 from database import insere_video
 
 
-for filename in os.listdir('coleta-youtube/Edit'):
-	with open(os.path.join('coleta-youtube/Edit', filename), 'r', encoding="utf8") as f:
-		
-		insere_video(filename.split('.')[0])
 
-		data = f.read()
-		a = []
-		b = {}
-		for line in data.splitlines():
-			if line:
-				if re.match(r'^([0-9]+:[0-9]+)$', line):
-					b["minute"] = int(line.split(':')[0])*60 + int(line.split(':')[1])
-					print(b["minute"])
-				else:
-					if 'line' in b:
-						b['line'] = b['line'] + " " + line
-					else:
-						b['line'] = line
-					
-			else:
-				if b:
-					a.append(b)
-					b = {}
-		if b:
-			a.append(b)
+
+for folder in os.listdir('coleta-youtube/transcripts/'):
+	for filename in os.listdir(os.path.join('coleta-youtube/transcripts/', folder,  'Edit')):
+		with open(os.path.join('coleta-youtube/transcripts/', folder, 'Edit', filename), 'r', encoding="utf8") as f:
+			
+			insere_video(filename.split('.')[0])
+
+			data = f.read()
+			a = []
 			b = {}
+			for line in data.splitlines():
+				if line:
+					if re.match(r'^([0-9]+:[0-9]+)$', line):
+						b["minute"] = int(line.split(':')[0])*60 + int(line.split(':')[1])
+						print(b["minute"])
+					else:
+						if 'line' in b:
+							b['line'] = b['line'] + " " + line
+						else:
+							b['line'] = line
+						
+				else:
+					if b:
+						a.append(b)
+						b = {}
+			if b:
+				a.append(b)
+				b = {}
 
-		insere_captions(a, os.path.splitext(filename)[0])
-	
-	# os.remove(os.path.join('captions', filename))
+			insere_captions(a, os.path.splitext(filename)[0])
+		
+		# os.remove(os.path.join('captions', filename))
 
