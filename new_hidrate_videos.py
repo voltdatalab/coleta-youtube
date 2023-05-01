287ccb3728e5ee34f835ca3753bbecfbf9785cc2
Lucas Lago <lago@nic.br>
2023-05-01 16:17:38 -0300

    rodando 2 de cada vez

--- new_hidrate_videos.py
+++ new_hidrate_videos.py
@@ -11,11 +11,16 @@ with open("coleta-youtube/config.json") as jsonfile:
 api = Api(api_key=config["key"])
 
 videos = lista_videos(False, 2)
+i = 0
 for video in videos:
+	if i > 2:
+		break
+	
 	try:
 		# source = pafy.new(video.yt_video_id, gdata=True)
 		source = api.get_video_by_id(video_id=video.yt_video_id)
 		update_video(source)
+		i += 1
 	except:
 		print("removido: " + video.yt_video_id) 
 		print()
