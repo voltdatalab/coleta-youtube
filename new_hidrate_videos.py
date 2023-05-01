f339629eb221149e213cb8de284526de195501bb
Lucas Lago <lago@nic.br>
2023-05-01 16:22:54 -0300

    removido limitador

--- new_hidrate_videos.py
+++ new_hidrate_videos.py
@@ -11,14 +11,9 @@ with open("coleta-youtube/config.json") as jsonfile:
 api = Api(api_key=config["key"])
 
 videos = lista_videos(False, 2)
-i = 0
 for video in videos:
-	if i > 2:
-		break
-	
 	try:
 		# source = pafy.new(video.yt_video_id, gdata=True)
-		i += 1
 		source = api.get_video_by_id(video_id=video.yt_video_id)
 		update_video(source)
 		
